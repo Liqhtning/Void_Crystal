96a3b97c221e66029fc4b0ef7d36f309970bcf2a
BuildTools <schoolsmarty@hotmail.com>
2022-07-17 15:47:27 -0500

    Initial commit

---
+++ src/main/java/net/liqht/void_crystal/Main.java
@@ -0,0 +1,14 @@
+package net.liqht.void_crystal;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main implements ModInitializer {
+	public static final String MOD_ID = "voidcrystal";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/liqht/void_crystal/MainClient.java
@@ -0,0 +1,10 @@
+package net.liqht.void_crystal;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MainClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
